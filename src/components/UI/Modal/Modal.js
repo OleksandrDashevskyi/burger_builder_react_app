88a3a2e9d4b68e614acf93277ced16e503bde33c
Oleksandr Dashevskyi <dashevskyi2013@gmail.com>
2020-11-03 00:23:37 -0800

    burger_builder

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Modal.css';
-import Auxiliary from "../../../hoc/Auxiliary";
+import Auxiliary from "../../../hoc/Auxiliary/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = (props) => {
-    return (
-        <Auxiliary>
-            <Backdrop show={props.show} clicked={props.modalClosed}/>
-            <div className='Modal'
-                 style={{
-                     transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                     opacity: props.show ? '1' : '0'
-                 }}>
-                {props.children}
-            </div>
-        </Auxiliary>
-    )
-};
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log('[Modal] WillUpdate')
+    }
+
+    render() {
+        return (
+            <Auxiliary>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className='Modal'
+                     style={{
+                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                         opacity: this.props.show ? '1' : '0'
+                     }}>
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        )
+    }
+}
 
 export default Modal;
